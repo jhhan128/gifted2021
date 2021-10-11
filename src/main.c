ad240cae26c4c181af4878efc100f80c485f2bec
Junhyuk Han <jhhan128@icloud.com>
2021-10-11 23:10:06 +0900

    .

--- src/main.c
+++ src/main.c
@@ -6,36 +6,35 @@
 #include "../lib/hash.h"
 
 
+void outputAll(const FILE *fp) {
+    char str[151];
+    
+    while (fgets(str, sizeof(str), fp) != NULL) {
+        fputs(str, stdout);
+    }
+}
+
+
 void firstExec(void) {
-    FILE *fp = fopen("../config.txt", "w");
+    char str[151];
+    FILE *fp, *output;
+    int counter = 1, a, b;
 
-    puts("gifted2021에 오신 것을 환영합니다.\n사용을 위해 할 일 목록을 만들어야 합니다.\n");
-    puts("이 프로그램에서 사용할 할 일 목록을 입력해 주세요.\n할 일의 이름은 줄바꿈으로 구분하고, 15글자 미만으로 해 주세요.");
-    puts("입력이 끝났다면 END를 입력해 주세요.\n");
+    fp = fopen("../config.txt", "w");
 
-    int number = 1;
-
+    outputAll(fopen("../lib/output/firstexec_1.txt", "r"));
+    
     while (true) {
-        printf("%d. ", number++);
-
-        char str[46];
+        printf("%d. ", counter++);
         fgets(str, sizeof(str), stdin);
-        str[strlen(str) - 1] = '\0';
 
-        if (strcmp(str, "END") == 0) break;
-
+        if (strcmp(str, "END\n") == 0) break;
         fputs(str, fp);
-        fputs("\n", fp);
     }
 
-    number--;
+    counter -= 2;
 
-    puts("\n이제 입력한 일들의 선후관계를 입력해야 합니다.");
-    puts("만약 a번 일을 b번 일보다 먼저 해야 한다면 a b와 같이 입력해 주세요.");
-    puts("줄바꿈으로 구분하고, 한 줄에는 수 두 개만 입력해 주세요.");
-    puts("입력이 끝났다면 END를 입력해 주세요.\n");
-
-    int a, b;
+    outputAll(fopen("../lib/output/firstexec_2.txt", "r"));
 
     while (true) {
         printf(">> ");
@@ -43,7 +42,7 @@ void firstExec(void) {
         int flag = scanf("%d %d", &a, &b);
         if (flag == EOF) break;
 
-        if (a > number || b > number) {
+        if (a > counter || b > counter) {
             puts("번호가 범위에 맞지 않습니다. 다시 입력해 주세요.");
             continue;
         }
@@ -51,8 +50,7 @@ void firstExec(void) {
         fprintf(fp, "%d %d\n", a, b);
     }
 
-    puts("감사합니다. gifted2021을 사용하기 위한 모든 준비가 끝났습니다.");
-    puts("이제부터는 프로그램을 다시 열면 사용하실 수 있습니다.");
+    outputAll(fopen("../lib/output/firstexec_3.txt", "r"));
 }
 
 
